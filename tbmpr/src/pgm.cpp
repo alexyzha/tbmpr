58522c838a223e1c7c55f4800913a86481447c3f
alexyzha <alexyzha@gmail.com>
2025-04-10 10:58:26 -0700

    fixed some logic issues

--- tbmpr/src/pgm.cpp
+++ tbmpr/src/pgm.cpp
@@ -2,7 +2,7 @@
 
 frame read_pgm(std::string file_path) {
     // File handle
-    std::fstream file(file_path);
+    std::fstream file(file_path, std::ios::in | std::ios::binary);
     if(!file.is_open()) {
         throw std::runtime_error("Invalid file path.");
     }
@@ -29,7 +29,7 @@ frame read_pgm(std::string file_path) {
     file.ignore();
     
     // Max gray values > 255 are unsupported (uint8_t -> 8 byte max = 0xff = 255)
-    if(tag != "P2" || tag != "P5" || max_gray > 255) {
+    if((tag != "P2" && tag != "P5") || max_gray > 255) {
         throw std::runtime_error("Invalid .pgm file.");
     }
 
@@ -37,7 +37,7 @@ frame read_pgm(std::string file_path) {
     uint8_t* img = frame_malloc(x, y, frame_format::GRAYSCALE);
     if(tag == "P2") {
         for(int i = 0; i < x; ++i) {
-            for(int j = 0; j < y; ++i) {
+            for(int j = 0; j < y; ++j) {
                 file >> img[i * x + y];
             }
         }
